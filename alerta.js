61f7e029b630af97ca3ff8540cbbde01ef153456
Henrique Santana <126524242+HenriqueeSantana@users.noreply.github.com>
2024-10-28 16:21:25 -0300

    Add files via upload

---
+++ alerta.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".tread").addEventListener("click", function(event) {
+        alert('Esta funcionalidade está em desenvolvimento!');
+        event.preventDefault();
+    });
+
+    document.querySelector(".meu-site").addEventListener("click", function(event) {
+        alert('Esta funcionalidade está em desenvolvimento!');
+        event.preventDefault();
+    });
+
+    document.querySelector(".meulink").addEventListener("click", function(event) {
+        alert('Esta funcionalidade está em desenvolvimento!');
+        event.preventDefault();
+    });
+});
